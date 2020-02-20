d1370109f1fca922958a1d998e2b5e21bebceef3
sceballosp <60147093+sceballosp@users.noreply.github.com>
2020-02-20 16:28:43 -0500

    Add files via upload

---
+++ talleres/taller05/Punto02.py
@@ -0,0 +1,16 @@
+import time
+t1 = time.time()
+
+
+def sumArray(array):
+    sum = 0
+    for i in array:
+        sum = sum + i
+    return sum
+
+
+print(sumArray([]))
+
+t2 = time.time()
+print(str(t2 - t1))
+
